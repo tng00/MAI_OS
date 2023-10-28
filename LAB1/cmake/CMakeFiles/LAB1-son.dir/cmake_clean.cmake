e81c72c20d1dabcbdd28b9a6a3be4230f6345d95
tng00 <tnguen00@gmail.com>
2023-10-28 04:41:22 +0300

    add lab1

---
+++ LAB1/cmake/CMakeFiles/LAB1-son.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LAB1-son.dir/home/tng00/OS/common/sub.c.o"
+  "CMakeFiles/LAB1-son.dir/home/tng00/OS/common/sub.c.o.d"
+  "CMakeFiles/LAB1-son.dir/src/son.c.o"
+  "CMakeFiles/LAB1-son.dir/src/son.c.o.d"
+  "LAB1-son"
+  "LAB1-son.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LAB1-son.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
